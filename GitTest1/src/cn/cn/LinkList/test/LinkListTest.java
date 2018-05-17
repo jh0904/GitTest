62a267c94d673a087cbf6ca326edd1cc16176f2a
jh0904 <464062919@qq.com>
2018-05-17 08:48:47 +0800

    单向链表实现增加，删除，插入，打印等功能。

---
+++ GitTest1/src/cn/cn/LinkList/test/LinkListTest.java
@@ -0,0 +1,10 @@
+package cn.cn.LinkList.test;
+
+import java.util.LinkedList;
+
+public class LinkListTest {
+    public static void main(String[] args) {
+        LinkedList l=new LinkedList();
+
+    }
+}
