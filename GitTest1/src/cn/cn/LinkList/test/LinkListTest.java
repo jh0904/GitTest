b6e4e2c93addc0b765b0f722d3fcf3a245ab0aa0
jh0904 <464062919@qq.com>
2018-07-06 15:03:21 +0800

    对LinkList的一些修改

--- GitTest1/src/cn/cn/LinkList/test/LinkListTest.java
+++ GitTest1/src/cn/cn/LinkList/test/LinkListTest.java
@@ -1,10 +1,32 @@
 package cn.cn.LinkList.test;
 
-import java.util.LinkedList;
+import com.sample.User;
+
+import java.util.*;
 
 public class LinkListTest {
-    public static void main(String[] args) {
-        LinkedList l=new LinkedList();
 
+    public static void main(String[] args) {
+      /*  List l = new LinkedList ();
+        ArrayList<Integer> list=new ArrayList<Integer> ();
+        list.add (1);
+        list.add (2);
+        list.add (3);
+        System.out.println (list.toString ());
+        User u=new User ();*/
+        LinkedHashSet  lhs = new LinkedHashSet ();
+        lhs.add (1);
+        lhs.add (2);
+        lhs.add (3);
+        lhs.add (4);
+        lhs.add (5);
+        lhs.add (6);
+        lhs.add (1);
+        for (Object lh : lhs) {
+            System.out.println (lh);
+        }
+        System.out.println ();
+        System.out.println ();
     }
+
 }
